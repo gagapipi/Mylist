12edf4b87d16705f736a820632b06578cbede69b
tomgagapipi <tominvyi@gmail.com>
2020-04-25 14:57:46 -0400

    Books Create/Index/New

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express') // get an insident of express object
 const router = express.Router()     // get an router obj from express
+const Book = require('../models/book')
 
-router.get('/', (req, res) => {
-    res.render('index')
+router.get('/', async (req, res) => {
+    let books
+    try {
+        books = await Book.find().sort({ createAt: 'desc'}).limit(10).exec()
+    } catch {
+        books = []
+    }
+    res.render('index', { books: books})
 })
 
 module.exports = router
