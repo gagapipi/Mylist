e8b907b624eeff4af37e7a61e5803178f7d73298
tomgagapipi <tominvyi@gmail.com>
2020-05-07 09:58:29 -0400

    Finished Author Routes

--- server.js
+++ server.js
@@ -7,15 +7,18 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-Override')
 
 const indexRouter = require('./routes/index') // tell server where the router is
 const authorRouter = require('./routes/authors')
-const bookRouter = require('./routes/books')
+const bookRouter = require('./routes/books');
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')  // create a views folder
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
