3d5e42efa1f2d6f071e99484a978c8613b413ff1
tomgagapipi <tominvyi@gmail.com>
2020-05-11 16:19:10 -0400

    5/11 update server.js, changed ln10 method-override

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
-const methodOverride = require('method-Override')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index') // tell server where the router is
 const authorRouter = require('./routes/authors')
